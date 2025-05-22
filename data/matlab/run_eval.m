5c177a62fb3cc43daef2651c3d9a04fb1e64e8cc
dogerasputin <113506248+dogerasputin@users.noreply.github.com>
2025-05-22 23:20:07 +0800

    Update requirements.txt

---
+++ data/matlab/run_eval.m
@@ -0,0 +1,7 @@
+addpath('D:/schoolwork/Machine_learning/LSCData/matlab');
+
+result_folder = 'D:\schoolwork\Machine_learning\LSCData\';
+gt_folder = 'D:\schoolwork\Machine_learning\LSCData\';
+
+LSC_Evaluation(result_folder, gt_folder);
+
